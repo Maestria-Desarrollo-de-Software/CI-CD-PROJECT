2fe1a40d537e362b19c7dde316ff98b3e6503834
Sergio Lema <sergio.lema.e@ucb.edu.bo>
2026-01-28 20:38:34 -0400

    Creacion de bug para verificar falla en codigo

--- src/main/java/org/springframework/samples/petclinic/api/HealthController.java
+++ src/main/java/org/springframework/samples/petclinic/api/HealthController.java
@@ -8,9 +8,7 @@ import org.springframework.jdbc.core.JdbcTemplate;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
-@SuppressFBWarnings(
-    value = "EI_EXPOSE_REP2",
-    justification = "JdbcTemplate is a Spring-managed dependency; not exposed outside.")
+@SuppressFBWarnings(value = "EI_EXPOSE_REP2", justification = "JdbcTemplate is a Spring-managed dependency; not exposed outside.")
 @RestController
 public class HealthController {
 
